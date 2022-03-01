08df56506d87f4c018e64d0f751a855d307f75a8
werdx64 <91020345+werdx64@users.noreply.github.com>
2022-03-01 17:13:59 +0300

    Update

--- ttyown.py
+++ ttyown.py
@@ -6,23 +6,9 @@ import glob
 availablePorts=[]
 
 while True:
-  ttySearch()
-  acmSerach()
+  for file in glob.glob("/dev/ttyUSB*"):
+    availablePorts.append(file)
 
-def ttySearch():
-  for tty in glob.glob("/dev/ttyUSB*"):
-    availablePorts.append(tty)
-
-  for usb in availablePorts:
-    if acm == "ttyUSB"*
-      os.system("chown werdx64 /dev/ttyUSB*")
-      exit()
-
-def acmSearch():
-  for acm in glob.glob("/dev/ttyACM*"):
-    availablePorts.append(acm)
-
-  for acm in availablePorts:
-    if acm == "ttyACM"*
-      os.system("chown werdx64 /dev/ttyACM*")
-      exit()
+  if len(availablePorts) > 0:
+    os.system("chown werdx64 /dev/ttyUSB*")
+    exit()
